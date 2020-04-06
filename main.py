424d6c34982d605b9cd0a691591bdf17a51ebcce
Paul Kassianik <pkassianik@gmail.com>
2020-04-06 11:00:39 -0700

    finally, working model

--- main.py
+++ main.py
@@ -5,19 +5,58 @@ from tensorflow import keras
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
 
+print("Shape of xs are: " + str(x_train.shape))
+
 output_size = 10
 
 # Create the model
+"""
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=x_train.shape[1:]))
+model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=x_train.shape[1:]))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+print(model.output_shape)
 model.add(tf.keras.layers.Conv2D(128, kernel_size=(3, 3), activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+print(model.output_shape)
+model.add(tf.keras.layers.Conv2D(256, kernel_size=(3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.BatchNormalization())
+print(model.output_shape)
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(512, activation='relu'))
-model.add(tf.keras.layers.Dense(256, activation='relu'))
+model.add(tf.keras.layers.Dense(output_size, activation='softmax'))
+"""
+
+weight_decay = 1e-4
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay), input_shape=x_train.shape[1:]))
+model.add(tf.keras.layers.Activation('elu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(32, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+model.add(tf.keras.layers.Activation('elu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+model.add(tf.keras.layers.Dropout(0.2))
+ 
+model.add(tf.keras.layers.Conv2D(64, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(64, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+model.add(tf.keras.layers.Dropout(0.3))
+ 
+model.add(tf.keras.layers.Conv2D(128, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.Conv2D(128, (3,3), padding='same', kernel_regularizer=tf.keras.regularizers.l2(weight_decay)))
+model.add(tf.keras.layers.Activation('relu'))
+model.add(tf.keras.layers.BatchNormalization())
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2,2)))
+model.add(tf.keras.layers.Dropout(0.4))
+ 
+model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(output_size, activation='softmax'))
 
 model.summary()
@@ -28,7 +67,7 @@ model.compile(optimizer = tf.keras.optimizers.Adam(),
 
 model.fit(x_train,
         y_train,
-        epochs = 7,
+        epochs = 50,
         verbose = 1,
         validation_split = 0.3
 )
